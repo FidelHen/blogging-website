26869082740dc3aa066070ba6e041b9c1d0f1c5e
fidelhen <fidelhen@gmail.com>
2020-12-31 15:06:37 -0500

    Adding routes for nav buttons and organize files

--- app.js
+++ app.js
@@ -15,10 +15,16 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-  res.render("home", {startingContent: homeStartingContent});
+  res.render("home", {homeContent: homeStartingContent});
 });
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactContent: contactContent});
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
