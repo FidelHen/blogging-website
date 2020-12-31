632514290713995f0f045a9452e30dae6feee0cf
fidelhen <fidelhen@gmail.com>
2020-12-31 15:33:40 -0500

    Add compose route and style page

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+//Routes
 app.get("/", function(req, res){
   res.render("home", {homeContent: homeStartingContent});
 });
@@ -26,6 +26,18 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactContent: contactContent});
 });
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+//Posts
+app.post("/compose", function(req, res){
+  var message = req.body.postMessage;
+
+  console.log(message);
+});
+
+//Server
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
